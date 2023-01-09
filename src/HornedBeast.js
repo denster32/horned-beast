9342bda5c03c139cf4962970f26f15f6da7ea24c
Dennis Palucki <paluckide@yahoo.com>
2023-01-09 14:56:22 -0600

    first push of code

---
+++ src/HornedBeast.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HornedBeast extends React.Component{
+ render(){
+   return(
+     <>
+      <h2></h2>
+      <img></img>
+      <p></p>
+     </>
+   )
+}
+}
+export default HornedBeast;
