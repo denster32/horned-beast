af3d7e0654197493da61e6dc2b5cdb519c19ebad
Dennis Palucki <paluckide@yahoo.com>
2023-01-15 01:59:16 -0600

    Add data files

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      waves: 0,
+    }
+  }
   render() {
     return (
       <>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.image_url} alt={this.props.description} title={this.props.title} />
-        <p>{this.props.description}</p>
+        <article>
+          <h2>{this.props.title}</h2>
+          <img src={this.props.image_url} alt={this.props.description} title={this.props.title} />
+          <p>{this.props.description}</p>
+        </article>
+
       </>
     )
   }
